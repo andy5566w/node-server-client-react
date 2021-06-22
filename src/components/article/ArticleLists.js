f3749d19a9327419ec6afd952e86c5c1158b58d6
andy tsai <andy5566w@gmail.com>
2021-06-22 20:07:04 +0800

    feat: 建立Article-lists裡面的header

--- src/components/article/ArticleLists.js
+++ src/components/article/ArticleLists.js
@@ -1,8 +1,37 @@
 import { useParams } from 'react-router-dom'
 import classes from './ArticleLists.module.scss'
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllArticles } from '../../store/action/Articles-action'
+
 const ArticleLists = () => {
+  const dispatch = useDispatch()
   const params = useParams()
-  return <div className={classes.article}>{params.id} ArticleListsItem</div>
+  const articles = useSelector((state) => state.articles.articles)
+  if (!articles.length) {
+    dispatch(getAllArticles())
+  }
+
+  const article = articles.find(({ _id }) => _id === params.id)
+  console.log(article)
+
+  return (
+    <div className={classes.article}>
+      <div
+        className={classes.article__header}
+        style={{ backgroundColor: article.color, color: article.color }}
+      >
+        <div className={classes.article__headerLeft}>
+          <h3>{article.title}</h3>
+          <h4 className="my-2">{article.sub_title}</h4>
+          <span>{article.tag}</span>
+        </div>
+
+        <div className={classes.article__headerRight}>
+          <p>作者:{article.author}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ArticleLists
