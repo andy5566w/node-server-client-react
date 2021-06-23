9d70bb0627cad3291dd7d29ec323f8cc6664c742
andy tsai <andy5566w@gmail.com>
2021-06-23 17:42:26 +0800

    style: ArticleLists 樣式調整

--- src/components/article/ArticleLists.js
+++ src/components/article/ArticleLists.js
@@ -3,6 +3,41 @@ import classes from './ArticleLists.module.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import { getAllArticles } from '../../store/action/Articles-action'
 
+const cards = [
+  {
+    id: 1,
+    title: 'JavaScript 訂閱模式',
+  },
+  {
+    id: 2,
+    title: 'Java 執行續介紹',
+  },
+  {
+    id: 3,
+    title: '如何打包一個程式',
+  },
+  {
+    id: 4,
+    title: '面試人生的心得',
+  },
+  {
+    id: 5,
+    title: '如何找到一個好的工作',
+  },
+  {
+    id: 6,
+    title: '幾招教你部署你理想的城市，幾招教你部署你理想的城市',
+  },
+  {
+    id: 7,
+    title: '我就只是一個測試',
+  },
+  {
+    id: 8,
+    title: '我就只是一個測試',
+  },
+]
+
 const ArticleLists = () => {
   const dispatch = useDispatch()
   const params = useParams()
@@ -12,7 +47,15 @@ const ArticleLists = () => {
   }
 
   const article = articles.find(({ _id }) => _id === params.id)
-  console.log(article)
+  if (!article) return <p>Loading</p>
+  const article_cards = cards.map(({ id, title }) => (
+    <div key={id}>
+      <div className={classes.dot} style={{ backgroundColor: article.color }} />
+      <h4>{title}</h4>
+      <i className={classes.comment + ' las la-comment-dots'} />
+      <i className={classes.heart + ' las la-heart'} />
+    </div>
+  ))
 
   return (
     <div className={classes.article}>
@@ -30,6 +73,8 @@ const ArticleLists = () => {
           <p>作者:{article.author}</p>
         </div>
       </div>
+
+      <div className={classes.article__lists}>{article_cards}</div>
     </div>
   )
 }
